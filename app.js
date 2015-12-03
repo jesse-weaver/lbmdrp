303580d3e6b376487f7b3f3b4c908aace9e84ed5
Matt James <mattjamesvisuals@gmail.com>
2015-12-03 09:20:19 -0700

    added new backgrounds

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ nunjucks.configure('views', {
 });
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
