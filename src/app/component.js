0e754e1aa547846ee4dad981d387ff2b8d088cae
Jesse Weaver <jesse.weaver@gmail.com>
2017-09-29 12:55:42 -0600

    Adding example react component

--- src/app/component.js
+++ src/app/component.js
@@ -1,5 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// var HelloComponent = React.createClass({
+//     render: function() {
+//         return <div>Hello </div>;
+//     }
+// });
+
+
+
+
+
 export default function() {
   const element = document.createElement('h3');
   element.innerHTML = 'do what the fuck you want!';
   return element;
+  // ReactDOM.render(HelloComponent, document.getElementById('app'))
 }
+
+
+// class Layout extends React.Component {
+//   render() {
+//     return (
+//       <div className="Main">
+//         <Header />
+//         <SearchBar>
+//           <SearchButton
+//             buttonType="big"
+//             />
+//         </SearchBar>
+//         <SearchResults />
+//       </div>
+//     );
+//   }
+// }
+
+
+//ReactDOM.render(Main, document.getElementById('app'))
--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import component from './component';
+import component from './component.js';
 import './main.css';
 
 document.body.appendChild(component());
