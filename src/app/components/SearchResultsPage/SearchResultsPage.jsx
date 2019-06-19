c108c19d8706e982a9407ce8f0d3ad6eff8bd2c4
Jesse Weaver <jweaver@shutterstock.com>
2019-06-19 12:03:55 -0600

    Update search results to functional component

--- src/app/components/SearchResultsPage/SearchResultsPage.jsx
+++ src/app/components/SearchResultsPage/SearchResultsPage.jsx
@@ -1,50 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Layout from '../Layout/Layout.jsx';
 import searchResultsPageCss from './SearchResultsPage.css';
 
-export default class SearchResultsPage extends Component {
-
-  static propTypes = {
-    searchResults: PropTypes.array.isRequired,
-  }
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { results } = this.props;
-    return (
-      <Layout
-        displaySearchBar={true}
-      >
-        <ul className="search-results">
-          {!this.props.searchResults.length && (
-            <div>No Artists Found</div>
-          )}
-          {this.props.searchResults.map(item => (
-            <li className="search-result-item" key={item.mkid}>
-              <span className="artist-thumbnail">
-                {item.image ? (
-                  <img src={item.image} />
-                ) : (
-                    <span className="placeholder-thumbnail">
-                      <img src="/images/searchResultRecord.png" />
-                    </span>
-                  )}
+const SearchResultsPage = ({ searchResults }) => (
+  <Layout displaySearchBar>
+    <ul className="search-results">
+      {!searchResults.length && (
+        <div>No Artists Found</div>
+      )}
+      {searchResults.map(item => (
+        <li className="search-result-item" key={item.mkid}>
+          <span className="artist-thumbnail">
+            {item.image ? (
+              <img src={item.image} />
+            ) : (
+              <span className="placeholder-thumbnail">
+                <img src="/images/searchResultRecord.png" />
               </span>
-              <Link className="link" to={`/artist/${item.mkid}`}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </Layout>
-    );
-  }
+            )}
+          </span>
+          <Link className="link" to={`/artist/${item.mkid}`}>{item.name}</Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+);
 
-  static defaultProps = {
-    searchResults: []
-  };
+SearchResultsPage.propTypes = {
+  searchResults: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
-}
+SearchResultsPage.defaultProps = {
+  searchResults: [],
+};
+
+export default SearchResultsPage;
